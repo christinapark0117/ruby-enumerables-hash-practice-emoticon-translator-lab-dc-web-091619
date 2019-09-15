925a814467d05419706ddc8da333cf643bb995ed
Christina Park <christinapark0117@gmail.com>
2019-09-15 20:23:20 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -10,8 +10,19 @@ def load_library(file_path)
   result
 end
 
-def get_japanese_emoticon
-  # code goes here
+def get_japanese_emoticon(yaml_file, emoticon)
+  library = load_library(yaml_file)
+  response = nil
+  library["get_emoticon"].each do |english, japanese|
+    if emoticon == english
+      response = japanese
+    end
+  end
+  if response == nil
+    return "Sorry, that emoticon was not found"
+  else
+    return response
+  end
 end
 
 def get_english_meaning
